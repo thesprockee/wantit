1b07fed938d5b1541c201c09881369b585b76f69
Michael Barrett <loki77@gmail.com>
2012-09-23 02:11:34 -0700

    Adding avatar
    http://pypi.python.org/pypi/django-avatar/1.0.5

--- wantit/settings.py
+++ wantit/settings.py
@@ -122,6 +122,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.admin',
+    'avatar',
     'django_extensions',
     'apps.campers',
     'apps.forums',
