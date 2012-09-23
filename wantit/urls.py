00f2905dfd349890e4be5dc2dc99551361c4f8bd
Michael Barrett <loki77@gmail.com>
2012-09-23 02:31:02 -0700

    Build index

--- wantit/urls.py
+++ wantit/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^avatar/', include('avatar.urls')),
+    url(r'^$', TemplateView.as_view(template='index.html'), name='index'),
 )
